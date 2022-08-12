087139fdf480c3c711a5e0d80c96a0474ff20360
uuuuuu <2622861754@qq.com>
2022-08-12 18:21:20 +0800

    douyu-live

--- src/main.tsx
+++ src/main.tsx
@@ -4,15 +4,15 @@ import App from './App'
 import 'font-awesome/css/font-awesome.min.css'
 import './assets/font/iconfont.css'
 import './rem'
-import { BrowserRouter } from 'react-router-dom'
+import { BrowserRouter,HashRouter } from 'react-router-dom'
 import { Provider } from 'react-redux'
 import store from './store'
 
 
 ReactDOM.createRoot(document.getElementById('root') as HTMLElement).render(
   <Provider store={store}>
-    <BrowserRouter>
+    <HashRouter>
       <App />
-    </BrowserRouter>
+    </HashRouter>
   </Provider>
 )
